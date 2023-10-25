e63261737117de82e161c47394c14ff67c3cbfc9
betrizbea <134522816+betrizbea@users.noreply.github.com>
2023-10-25 11:50:06 -0300

    up

--- main.js
+++ main.js
@@ -8,3 +8,38 @@ function TocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
 document.querySelector('.tecla_clap').onclick = tocaSomclap;
+
+function TocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function TocaSomPuff(){
+    document.querySelector('#som_tecla_Puff').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSompuff;
+
+function TocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomsplash;
+
+function TocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick = tocaSomtoim;
+
+function TocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh').onclick = tocaSompsh;
+
+function TocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+ }
+ document.querySelector('.tecla_tic').onclick = tocaSomtic;
+
+ function TocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclick = tocaSomtom;
